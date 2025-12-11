8988b81305fe4786aadcfaca1f719fb9237b16ca
Priyanshu Midha <155522896+PriyanshuMidha@users.noreply.github.com>
2025-12-12 01:47:04 +0530

    bug fix for froned

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,9 @@ import { Fragment, useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Fragment>
-      <div>First push </div>
+      <div>second push </div>
     </Fragment>
   )
 }
